3abce1584e13e8080c255305321bf28dc97383ed
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-08-04 10:56:13 -0400

    Create initializefile.sql

---
+++ SQL Scripts/initializefile.sql
@@ -0,0 +1,16 @@
+
+
+-- Create database 'Data Warehouse'
+
+USE master;
+
+create database DataWarehouse;
+
+
+use DataWarehouse;
+
+create schema bronze;
+create schema silver;
+go
+create schema gold;
+go
